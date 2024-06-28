ce2f0fdcc2263fdc3c18761f86bb41204b9267d1
RafaelFreimuth <rafaelfreimuth@outlook.com>
2024-06-28 20:23:38 -0300

    Add express e initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Initial ")
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("Get route");
+
+    return res.send("Hello Word");
+});
+
+app.listen(3333, () => {
+    console.log("Server is running");
+});
