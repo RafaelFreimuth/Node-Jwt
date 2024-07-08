cf9f4d8e8fa73d7ae992cf754f84a6047abe89d3
RafaelFreimuth <rafaelfreimuth@outlook.com>
2024-07-08 20:43:08 -0300

    Using environment variables

--- src/configuration/datasource/data-source.ts
+++ src/configuration/datasource/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "node-jwt",
+    host: process.env.TYPEORM_HOST,
+    port: parseInt(process.env.TYPEORM_PORT || "8080"),
+    username: process.env.TYPEORM_USER,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
     synchronize: true,
     logging: false,
     entities: ["**/models/**/*.js"],
