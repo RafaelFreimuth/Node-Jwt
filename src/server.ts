bc4cb2a6521738788ba200b6af18c259ff1b7d16
RafaelFreimuth <rafaelfreimuth@outlook.com>
2024-07-08 20:42:15 -0300

    Melhorando legibilidade de codigo

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
-import express, { NextFunction } from 'express';
+import express from 'express';
 import { AppDataSource } from './configuration/datasource/data-source';
 import { userRoute } from './routes/UserRoute';
 
 const app = express();
 
-AppDataSource.initialize().then(() => {
-    console.log("sucess database");
-});
-
 app.use(express.json());
 app.use('/user', userRoute);
 
+AppDataSource.initialize().then(() => {
+    console.log("sucess database");
+});
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log("Server is running...");
