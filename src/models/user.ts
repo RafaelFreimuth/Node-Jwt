e95476c6d328c169f050f1e8b5d73980bd03579e
RafaelFreimuth <rafaelfreimuth@outlook.com>
2024-07-08 20:43:48 -0300

    Removing unused import

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export default class User {
